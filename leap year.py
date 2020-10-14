e5b706756bbeb3b157bb18cda0fe1255de51e81e
RamachandiranVijayalakshmi <66218468+RamachandiranVijayalakshmi@users.noreply.github.com>
2020-10-14 20:38:30 +0530

    Add files via upload

---
+++ leap year.py
@@ -0,0 +1,13 @@
+def value(year):
+    if year%4==0:
+        if year%100==0:
+            if year%400==0:
+               print(year,'is a Leap Year')
+            else:
+               print(year, "is not a Leap Year")
+        else:
+            print(year,'is a Leap Year')
+    else:
+        print(year, "is not a Leap Year")
+year = int(input('Enter year:'))
+value(year)
